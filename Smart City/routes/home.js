9d024b6eb8f313b0c3917d58dbbdb3d20c25f671
Amit Pandey <amitpandeysjsu@gmail.com>
2017-05-13 14:23:07 -0700

    initial files

---
+++ Smart City/routes/home.js
@@ -0,0 +1,40 @@
+
+var winston = require('winston');
+
+var logger = new (winston.Logger)({
+	transports: [
+		new (winston.transports.Console)(),
+		new (winston.transports.File)({ filename: 'ebayLog.log' })
+	]
+});
+
+exports.redirectToHome = function(req,res) {
+    res.render('login');
+};
+
+exports.redirectToDashboard= function(req,res) {
+    res.render('index');
+};
+
+exports.redirectToUserProfile = function(req,res) {
+    res.render('feed');
+};
+
+exports.redirectToAdminProfile = function(req,res) {
+    res.render('admin');
+};
+
+exports.redirectToProfileCompletion = function(req,res) {
+    res.render('completeProfile');
+};
+exports.redirectToforgotPassword=function(req,res){
+    res.render('forgotpassword');
+}
+
+exports.redirectToEvents=function(req,res){
+    res.render('Events');
+}
+exports.redirectToAccomodation=function(req,res){
+    res.render('accomodation');
+}
+
